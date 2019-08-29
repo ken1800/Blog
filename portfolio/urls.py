35637c287b547f6e67a25540dcbe92b7b0b0467c
kenneth irungu <kennethirungu1800@gmail.com>
2019-08-29 12:46:15 -0400

    edited the templates

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('',views.index,name='index'),
     path('about',views.about,name='about'),
+    path('contact',views.contact,name='contact'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -4,5 +4,8 @@ def index(request):
     return render(request, 'index.html',{})
 def about(request):
     return render(request, 'about.html',{})
+def contact(request):
+    return render(request, 'contact.html',{})
+
 
 # Create your views here.
