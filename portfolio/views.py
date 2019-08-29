3e3a04aa69e17351f566f9a75c96250fa58c3c30
kenneth irungu <kennethirungu1800@gmail.com>
2019-08-29 12:21:07 -0400

    dynamically used jinja 2 to my templates

--- portfolio/views.py
+++ portfolio/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html',{})
+def about(request):
+    return render(request, 'about.html',{})
 
 # Create your views here.
