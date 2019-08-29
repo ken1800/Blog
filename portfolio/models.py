56a8f2b2c8a67c493ecfc692da31f04a19423f27
kenneth irungu <kennethirungu1800@gmail.com>
2019-08-29 13:05:05 -0400

    created home model that will handle home data

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from . import views
+class Home(models.Model):
+    topic =models.CharField(max_length=200)
+    information = models.TextField(max_length=600)
+
+    def __str__(self):
+        return self.topic+"-"+str(self.information)
+    
+    
 
 # Create your models here.
