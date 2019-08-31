397a7dd5e9ba3311ed4a037bf15872dae27eaf0d
kenneth irungu <kennethirungu1800@gmail.com>
2019-08-31 14:11:08 -0400

    dynamically added the home home's text field

--- portfolio/models.py
+++ portfolio/models.py
@@ -9,7 +9,12 @@ class Home(models.Model):
         return self.topic+"-"+str(self.information)
     
 class TextHome(models.Model):
-    text1 = models.TextField(max_length=400)  
+    text1_h =models.CharField(max_length=200,default="")
+    text1 = models.TextField(max_length=400) 
+    text2_h =models.CharField(max_length=200,default="")  
     text2 = models.TextField(max_length=500)
+    
+    def __str__(self):
+        return self.text1+"-"+str(self.text1_h)
 
 # Create your models here.
--- portfolio/views.py
+++ portfolio/views.py
@@ -3,8 +3,10 @@ from . import models
 
 def index(request):
     home = models.Home.objects.order_by("-id")[:1]
+    Text = models.TextHome.objects.all()
+    
    # news = News.objects.order_by("-date")[:10]
-    return render(request, 'index.html',{"home": home})
+    return render(request, 'index.html',{"home": home,"Text":Text,})
 def about(request):
     return render(request, 'about.html',{})
 def contact(request):
