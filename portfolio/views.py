88cedaddf6b39a22936edfbf19b94ed4d1050102
kenneth irungu <kennethirungu1800@gmail.com>
2019-08-31 07:36:34 -0400

    modified the query

--- portfolio/views.py
+++ portfolio/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from . import models
 
 def index(request):
-    home = models.Home.objects.all()
-    
+    home = models.Home.objects.order_by("-id")[:1]
+   # news = News.objects.order_by("-date")[:10]
     return render(request, 'index.html',{"home": home})
 def about(request):
     return render(request, 'about.html',{})
