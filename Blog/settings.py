2f05bdce560cacedc88f2b65f7bc38e6efc9e0b1
kenneth irungu <kennethirungu1800@gmail.com>
2019-09-08 13:56:23 -0400

    editing

--- Blog/settings.py
+++ Blog/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 EMAIL_HOST           = 'smtp.gmail.com'
 EMAIL_PORT           = 587
 EMAIL_HOST_USER      = 'kennethirungu1800@gmail.com'
-EMAIL_HOST_PASSWORD  = 'proff1800'
+EMAIL_HOST_PASSWORD  = '*********'
 EMAIL_USE_TLS        = True
 EMAIL_BACKEND        = 'django.core.mail.backends.smtp.EmailBackend'
 
